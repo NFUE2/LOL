d1c71f20a6fad63a0432a6b4a470372a362658ab
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2022-06-23 00:26:24 +0900

    줌인/줌아웃

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    Transform Player;
+
+    Vector3 Offset_Pos;
+    Vector3 Follow_Camera_Pos;
+    Vector3 Zoom_Pos;
+
+    bool CameraFix;
+
+    [Range(1.0f,10.0f)]
+    public float Zoom_Distance;
+    float Zoom;
+
+    [Range(1.0f,10.0f)]
+    public float Zoom_Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Zoom = 0.0f;
+        Player = GameObject.Find("Player").transform;
+        CameraFix = false;
+        Offset_Pos = transform.position;
+        Follow_Camera_Pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        Cursor.lockState = CursorLockMode.Confined;
+
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            if (!CameraFix) CameraFix = true;
+            else CameraFix = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space)) CameraFix = true;
+
+        else if (Input.GetKeyUp(KeyCode.Space)) CameraFix = false;
+
+        float Z = Input.GetAxis("Mouse ScrollWheel") * Zoom_Speed;
+
+        if (Z > 0.0f)
+            Zoom = Mathf.Lerp(Zoom, Zoom_Distance, Z);
+
+        else if (Z < 0.0f)
+            Zoom = Mathf.Lerp(Zoom,0.0f,-Z);
+
+        Zoom_Pos = transform.forward * Zoom;
+        if (CameraFix)
+            Follow_Camera_Pos = Player.position + Offset_Pos;
+
+        else
+        {
+            if (Input.mousePosition.x < 10.0f)
+                Follow_Camera_Pos += Vector3.left * 10.0f * Time.deltaTime;
+
+            else if (Input.mousePosition.x > (Screen.width - 10.0f))
+                Follow_Camera_Pos += Vector3.right * 10.0f * Time.deltaTime;
+
+            if (Input.mousePosition.y < 10.0f)
+                Follow_Camera_Pos += Vector3.back * 10.0f * Time.deltaTime;
+
+            else if (Input.mousePosition.y > (Screen.height - 10.0f))
+                Follow_Camera_Pos += Vector3.forward * 10.0f * Time.deltaTime;
+        }
+
+        transform.position = Follow_Camera_Pos + Zoom_Pos;
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMove : MonoBehaviour
+{
+    //NavMeshAgent자료형의 변수명Agent를 선언
+    NavMeshAgent Agent; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Agent에 이 스크립트가 들어가게될 게임오브젝트에서 NavMeshAgent를 가져옵니다.
+        Agent = GetComponent<NavMeshAgent>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //게임화면에서 나의 마우스 위치로 Ray를 생성합니다.
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //마우스 우클릭 및 Ray의 끝에 충돌체가 있다면 그 지점을 읽어와서 Hit에 저장합니다.
+        if (Input.GetMouseButtonDown(1) && Physics.Raycast(ray,out RaycastHit Hit))
+        {
+            //Ray가 충돌되었던 위치를 도착지점으로 설정하고 이동하게됩니다.
+            Agent.destination = Hit.point;
+        }
+    }
+}
